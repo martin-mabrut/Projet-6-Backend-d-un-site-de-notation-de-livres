c75482c732ae5256f90ba35ebc708dd16273033a
Martin Mabrut <martinmabrut43@gmail.com>
2025-11-10 01:32:45 +0100

    Installation d'Express + test d'une première requête get

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.get('/api/books', (req, res) => {
+  const books = [
+    {
+      userId: 'Martin',
+      title: 'Harry Potter',
+      author: 'J.K. Rowling',
+      imageUrl: "URL de l'image",
+      year: 1997,
+      genre: 'SF',
+      ratings: [
+        { userId: 'Martin', grade: 5 }
+      ],
+      averageRating: 5
+    }
+  ];
+
+  res.status(200).json(books);
+});
+
+module.exports = app;
