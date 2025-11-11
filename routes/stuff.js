904049110a4bef571ea274cc6f75b8a3a2c33d0c
Martin Mabrut <martinmabrut43@gmail.com>
2025-11-11 16:11:38 +0100

    Séparation des routes et des controlleurs

---
+++ routes/stuff.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const stuffCtrl = require('../controllers/stuff');
+
+router.post('/', stuffCtrl.createBook);
+router.get('/:id', stuffCtrl.getOneBook);
+router.put('/:id', stuffCtrl.modifyBook);
+router.delete('/:id', stuffCtrl.deleteBook);
+router.get('/', stuffCtrl.getAllBooks);
+
+module.exports = router;
